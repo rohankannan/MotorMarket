7a871f8a14c1ed02cec84025de35f879a5e15e32
lymis321 <lschmuc@purdue.edu>
2025-03-27 15:01:15 -0400

    added a constructor

--- Listing.java
+++ Listing.java
@@ -1,10 +1,29 @@
 // listing class
 public class Listing implements ListingInterface {
   // fields
+  private String sellerUsername;
+  private String photoURL;
+  private String carType;
+  private String color;
+  private int mileage;
+  private int accidents;
+  private double price;
+  private boolean manual;
 
   // constructor
-
+  public Listing(String sellerUsername, String photoURL, String carType, String color, int mileage, int accidents, double price, boolean manual) {
+    this.sellerUsername = sellerUsername;
+    this.photoURL = photoURL;
+    this.carType = carType;
+    this.color = color;
+    this.mileage = mileage;
+    this.accidents = accidents;
+    this.price = price;
+    this.manual = manual;
+  }
+  
   // getters
+  
 
   // setters
 
