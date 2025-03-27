bee78a3e5645624ca27d73936d74bbe2426fda14
lymis321 <lschmuc@purdue.edu>
2025-03-27 14:40:00 -0400

    getters and setters added

--- UserInterface.java
+++ UserInterface.java
@@ -1,4 +1,20 @@
 
 public Interface UserInterface {
+  
+  // getters
+  String getUsername();
+  String getPassword();
+  String getAddress();
+  double getBalance();
+  ArrayList<Listing> getListings();
+  
+  // setters
+  void setUsername(String username);
+  void setPassword(String password);
+  void setAddress(String address);
+  void setBalance(double balance);
 
+  // update array by adding a new listing 
+
+}
 }
