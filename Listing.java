1a96a26e8c19cf82ce1ed436ef413820547fcfb0
xdsmt <stephen.tushentsov@gmail.com>
2025-03-27 15:07:09 -0400

    toString + formatting listing class
    added toString method and reformatted everything to be indented properly (for coding style)

--- Listing.java
+++ Listing.java
@@ -1,25 +1,25 @@
 // listing class
 public class Listing implements ListingInterface {
   // fields
-  private String sellerUsername;
-  private String photoURL;
-  private String carType;
-  private String color;
-  private int mileage;
-  private int accidents;
-  private double price;
-  private boolean manual;
+    private String sellerUsername;
+    private String photoURL;
+    private String carType;
+    private String color;
+    private int mileage;
+    private int accidents;
+    private double price;
+    private boolean manual;
 
   // constructor
-  public Listing(String sellerUsername, String photoURL, String carType, String color, int mileage, int accidents, double price, boolean manual) {
-    this.sellerUsername = sellerUsername;
-    this.photoURL = photoURL;
-    this.carType = carType;
-    this.color = color;
-    this.mileage = mileage;
-    this.accidents = accidents;
-    this.price = price;
-    this.manual = manual;
+    public Listing(String sellerUsername, String photoURL, String carType, String color, int mileage, int accidents, double price, boolean manual) {
+        this.sellerUsername = sellerUsername;
+        this.photoURL = photoURL;
+        this.carType = carType;
+        this.color = color;
+        this.mileage = mileage;
+        this.accidents = accidents;
+        this.price = price;
+        this.manual = manual;
   }
   
   // getters
@@ -28,4 +28,9 @@ public class Listing implements ListingInterface {
   // setters
 
   //other methods
+
+    public String toString() {
+        return sellerUsername + "," + photoURL + "," + carType + "," + color + ","
+            + mileage + "," + accidents + "," + price + "," + manual;
+    }
 }
