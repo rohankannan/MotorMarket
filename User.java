c8f277c174151ee63a1877a9a85755472eb31b32
ajprjx <alistairjoseph@pal-nat186-72-141.itap.purdue.edu>
2025-03-27 14:33:10 -0400

    added getters/setters to User

--- User.java
+++ User.java
@@ -19,6 +19,46 @@ public class User implements UserInterface {
 
   // getters
 
-  // setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public Listing[] getListings() {
+        return listings;
+    }
+
+    public void setListings(Listing[] listings) {
+        this.listings = listings;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    // setters
 
 }
