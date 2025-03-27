7c2e8cf4a4511c2f969b23d99b42f86c15415bbe
lymis321 <lschmuc@purdue.edu>
2025-03-27 14:57:14 -0400

    added methods to ListingInterface.java

--- ListingInterface.java
+++ ListingInterface.java
@@ -1,4 +1,25 @@
 
 public Interface ListingInterface {
 
+  // getters
+  String getPhotoURL();
+  String getCarType();
+  String getColor();
+  String getSeller();
+  int getMileage();
+  int getAccidents();
+  double getPrice();
+  boolean isManual();
+  
+  // setters
+  void setPhotoURL(String url);
+  void setCarType(String carType);
+  void setColor(String color);
+  void setSeller(String seller);
+  void setMileage(int mileage);
+  void setAccidents(int accidents);
+  void setPrice(double price);
+  void setManual(boolean manual);
+
+
 }
