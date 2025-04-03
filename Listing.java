d2e35dc2f9514d51d60799fe88eea28b4c2fb2ed
lymis321 <lschmuc@purdue.edu>
2025-04-03 14:04:12 -0400

    Update Listing.java, comments for methods

--- Listing.java
+++ Listing.java
@@ -15,7 +15,11 @@ public class Listing implements ListingInterface {
     private double price;            // the current price of this listing
     private boolean manual;          // whether or not the car being sold is manual
 
-  // constructor
+  /** constructor for Listing objects
+  * 9 parameters for each of the 9 attributes of a Listing object
+  * in format: String, String, String, String, int, int, double, boolean, String
+  * values: username, URL for display photo, type of car(make/model), color of car, mileage, how many accidents, current price, if it is manual, and unique ID number for the listing
+  */
     public Listing(String sellerUsername, String photoURL, String carType, String color, int mileage, int accidents, double price, boolean manual, String listingID) {
         this.sellerUsername = sellerUsername;
         this.photoURL = photoURL;
@@ -29,7 +33,9 @@ public class Listing implements ListingInterface {
   }
   
   // getters
-  
+  /** the getter/accessor methods for Listing objects
+  * returns the value of each attribute of a Listing object respectively
+  */
     public String getListingID() {
       return listingID;
     }
@@ -66,42 +72,78 @@ public class Listing implements ListingInterface {
     }
   
   // setters
+  /** the setter methods for Listing objects
+  * updates/alters the value of each attribute of a Listing object respectively
+  * one parameter: the new value that the variable is being set to 
+  */
+  
+  /** one parameter
+  * a String that represents the new/updated ID 
+  */
     public void setListingID(String id) {
       listingID = id;
     }
+  
+  /** one parameter
+  * a String that represents the new/updated URL 
+  */
     public void setPhotoURL(String url) {
       photoURL = url;
     }
 
+  /** one parameter
+  * a String that represents the new/updated type of car 
+  */
     public void setCarType(String carType) {
       this.carType = carType;
     }
 
+  /** one parameter
+  * a String that represents the new/updated color of the car 
+  */
     public void setColor(String color) {
       this.color = color;
     }
 
+  /** one parameter
+  * a String that represents the new/updated username of the seller 
+  */
     public void setSeller(String sellerUsername) {
       this.sellerUsername = sellerUsername;
     }
 
+  /** one parameter
+  * an int that represents the new/updated mileage 
+  */
     public void setMileage(int mileage) {
       this.mileage = mileage;
     }
 
+  /** one parameter
+  * an int that represents the new/updated amount of accidents 
+  */
     public void setAccidents(int accidents) {
       this.accidents = accidents;
     }
 
+  /** one parameter
+  * a double that represents the new/updated price 
+  */
     public void setPrice(double price) {
       this.price = price;
     }
-    
+
+  /** one parameter
+  * a boolean that represents the new/updated type of transmission (true: manual, false: automatic) 
+  */
     public void setManual(boolean manual) {
       this.manual = manual;
     }
 
   //other methods
+  /** toString method
+  * returns a string representation of this Listing object
+  */
   @Override
     public String toString() {
         return sellerUsername + "," + photoURL + "," + carType + "," + color + ","
