54c104ddc685d284ed6fd37743a40c8378ca469b
rohankannan <kannan36@purdue.edu>
2025-04-03 14:23:35 -0400

    wrote method headers and created new class request.java

---
+++ DatabaseInterface.java
@@ -0,0 +1,10 @@
+public interface DatabaseInterface {
+    String userfile = "userdata.txt";
+    String listingfile = "listingdata.txt";
+
+    void writeUserData(); // method to write user data to file
+    void writeListingData(); // method to write listing data to file
+    User[] readUserData(); // method to read user data from file and return array of User objects
+    Listing[] readListingData(); // method to read listing data from file and return array of Listing objects
+
+}
