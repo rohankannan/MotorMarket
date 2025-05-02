69f75ac69aa6e52cabce0917ea379f72ffab2a76
lymis321 <lschmuc@purdue.edu>
2025-05-02 18:13:57 -0400

    Create HubPanelInterface.java

---
+++ src/main/java/com/cs180proj/app/HubPanelInterface.java
@@ -0,0 +1,23 @@
+// imports
+package com.cs180proj.app;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * CS 18000 Group Project
+ *
+ * Interface for Client class which contains all
+ * methods and fields.
+ *
+ * @author (Rohan Kannan, Alistair Joseph,
+ * Lydia Schmucker, Stephen Tushentsov) lab sec 19
+ *
+ * @version May 2, 2025
+ */
+
+ public interface HubPanelInterface {
+
+    JPanel centerAlign(JComponent comp);
+    
+ }
