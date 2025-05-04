cb461491889d67f8530e95bf613591e438f6a3b3
xdsmt <stephen.tushentsov@gmail.com>
2025-05-04 16:25:59 -0400

    Updated ServerTestCase to work with new server changes

--- src/test/java/com/cs180proj/app/ServerTestCase.java
+++ src/test/java/com/cs180proj/app/ServerTestCase.java
@@ -1,10 +1,7 @@
 package com.cs180proj.app;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
@@ -24,12 +21,13 @@ import org.junit.jupiter.api.Test;
 public class ServerTestCase {
     
     /**  
-    * Test cases for isServerRunning() method, startServer() method, 
+    * Test case for isServerRunning() method, startServer() method, 
     * stopServer() method, constructor, and fields of Server class.
+    * Server also contains private class, so sending commands is also
+    * tested within this test case
     */
-    // Test case for isServerRunning() method, startServer() method, stopServer() method, constructor, and fields
     @Test
-    public void testIsServerRunningStartingStopping() throws Exception {
+    public void testIsServerRunningStartingStoppingSendCommand() throws Exception {
         Server server = new Server();
         Thread serverThread = new Thread(new Runnable() {
             public void run() {
@@ -41,60 +39,22 @@ public class ServerTestCase {
         });
         serverThread.start();
         Thread.sleep(150);
-        assertEquals(true, server.isServerRunning(), "Server should not be running after stopServer() is called.");
+        assertEquals(true, server.isServerRunning());
+        Socket testSocket = new Socket("localhost", 8080);
+        ObjectOutputStream testOOS = new ObjectOutputStream(testSocket.getOutputStream());
+        testOOS.flush();
+        ObjectInputStream testOIS = new ObjectInputStream(testSocket.getInputStream());
+        testOOS.writeObject("GET_USERS");
+        testOOS.flush();
+        Object r = testOIS.readObject();
+        assertEquals(true, r instanceof ArrayList);
+        testOOS.writeObject("EXIT");
+        testOOS.flush();
+        testOIS.close();
+        testOOS.close();
+        testSocket.close();
         server.stopServer();
         serverThread.join(1000);
-        assertEquals(false, server.isServerRunning(), "Server should not be running after stopServer() is called.");
+        assertEquals(false, server.isServerRunning());
     }
-
-    /**  
-    * Test cases for checkClientCommand() & workWithClient() methods
-    */
-    // Test case for checkClientCommand() method, uses the GET_USERS command
-    @Test
-    public void testCheckClientCommand() throws Exception {
-        Server server = new Server();
-        ByteArrayOutputStream testOut = new ByteArrayOutputStream();
-        ObjectOutputStream testOOS = new ObjectOutputStream(testOut);
-        testOOS.writeObject("test");
-        testOOS.flush();
-        ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
-        ObjectOutputStream oos = new ObjectOutputStream(byteOut);
-        ByteArrayInputStream byteIn = new ByteArrayInputStream(testOut.toByteArray());
-        ObjectInputStream ois = new ObjectInputStream(byteIn);
-        server.checkClientCommand("GET_USERS", ois, oos);
-        oos.flush();
-        ObjectInputStream rs = new ObjectInputStream(new ByteArrayInputStream(byteOut.toByteArray()));
-        Object r = rs.readObject();
-        assertEquals(true, r instanceof ArrayList);
-    }
-
-    // Test case for workWithClient() method, also tests exitability 
-    @Test
-    public void testWorkWithClient() throws Exception {
-        Server server = new Server();
-        ServerSocket serverSocket = new ServerSocket(8080);
-        int currentPort = serverSocket.getLocalPort();
-        Thread serverThread = new Thread(new Runnable() {
-            public void run() {
-                try {
-                    Socket socket = serverSocket.accept();
-                    server.workWithClient(socket);
-                } catch (Exception e) {
-                }
-            }
-        });
-        serverThread.start();
-        Socket socketTwo = new Socket("localhost", currentPort);
-        ObjectOutputStream oos = new ObjectOutputStream(socketTwo.getOutputStream());
-        oos.flush();
-        ObjectInputStream ois = new ObjectInputStream(socketTwo.getInputStream());
-        oos.writeObject("EXIT");
-        oos.flush();
-        socketTwo.close();
-        serverSocket.close();
-        serverThread.join(1000);
-        assertEquals(true, socketTwo.isClosed() && !serverThread.isAlive());
-    }
-
 }
